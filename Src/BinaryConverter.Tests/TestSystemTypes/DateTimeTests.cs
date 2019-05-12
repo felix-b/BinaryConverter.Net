fc9d46d2eccd16fe67ca1740c9ea7ee82049f523
aviko <avikenigsberg@gmail.com>
2019-05-12 17:49:50 +0300

    Add System Types Test

---
+++ Src/BinaryConverter.Tests/TestSystemTypes/DateTimeTests.cs
@@ -0,0 +1,24 @@
+﻿using BinaryConverter.Serializers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryConverter.Tests.TestSystemTypes
+{
+    [TestClass]
+    public class DateTimeTests
+    {
+        [TestMethod]
+        public void Test_DateTime()
+        {
+            var settings = new SerializerSettings();
+            settings.SerializerArgMap[typeof(DateTime)] = new DateTimeSerializerArg() { TickResolution = 1 };
+
+            var val = DateTime.Now;
+            var buf = BinaryConvert.SerializeObject(val, settings);
+            var cloned = BinaryConvert.DeserializeObject<DateTime>(buf, settings);
+            Assert.AreEqual(val, cloned);
+        }
+    }
+}
---
+++ Src/BinaryConverter.Tests/TestSystemTypes/StringTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryConverter.Tests.TestSystemTypes
+{
+    [TestClass]
+    public class StringTests
+    {
+        [TestMethod]
+        public void Test_String()
+        {
+            var val = "Hello World";
+            var buf = BinaryConvert.SerializeObject(val);
+            var cloned = BinaryConvert.DeserializeObject<string>(buf);
+            Assert.AreEqual(val, cloned);
+        }
+
+        [TestMethod]
+        public void Test_StringNull()
+        {
+            string val = null;
+            var buf = BinaryConvert.SerializeObject(val);
+            var cloned = BinaryConvert.DeserializeObject<string>(buf);
+            Assert.AreEqual(val, cloned);
+        }
+    }
+}
