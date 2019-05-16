f432a54120f9a3fa1ab170b7e1f335e9a4a842f3
aviko <avikenigsberg@gmail.com>
2019-05-16 10:14:52 +0300

    add more tests

---
+++ Src/BinaryConverter.Tests/Models/PocoComplex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryConverter.Tests.Models
+{
+    public enum TestEnum
+    {
+        Val0,
+        Val1,
+        Val10 = 10
+    }
+
+    class PocoComplex
+    {
+        public int Id { get; set; }
+        public DateTime Time1 { get; set; }
+        public DateTime Time2 { get; set; }
+        public string Comment { get; set; }
+        public PocoWithAllPrimitives SubRecord { get; set; }
+        public decimal Dec1 { get; set; }
+        public float Real32 { get; set; }
+        public double Real64 { get; set; }
+        public TestEnum TestEnum { get; set; }
+
+        public List<int> IntList { get; set; }
+        public List<PocoWithAllPrimitives> SubRecordList { get; set; }
+        public Dictionary<int, int> IntDict { get; set; }
+        public Dictionary<string, PocoSimple> SubRecordDict { get; set; }
+
+        public override bool Equals(object other)
+        {
+            var bufThis = BinaryConvert.SerializeObject(this);
+            var bufOhter = BinaryConvert.SerializeObject(other as PocoComplex);
+
+            return ArrayEqualityComparer<byte>.Equals(bufThis, bufOhter);
+        }
+
+        public override int GetHashCode()
+        {
+            var bufThis = BinaryConvert.SerializeObject(this);
+            return ArrayEqualityComparer<byte>.GetHashCode(bufThis);
+        }
+    }
+}
---
+++ Src/BinaryConverter.Tests/TestClasses/PocosTests.cs
@@ -0,0 +1,140 @@
+﻿using BinaryConverter.Tests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryConverter.Tests.TestClasses
+{
+    [TestClass]
+    public class PocosTests
+    {
+        [TestInitialize]
+        public void testInit()
+        {
+            SerializerRegistry.UnregisterClassMap(typeof(PocoSimple));
+        }
+
+
+        [TestMethod]
+        public void Test_PocosNull()
+        {
+            PocoSimple val = null;
+
+            var buf = BinaryConvert.SerializeObject(val);
+            var cloned = BinaryConvert.DeserializeObject<PocoSimple>(buf);
+
+            Assert.AreEqual(val, cloned);
+        }
+
+        [TestMethod]
+        public void Test_PocosSimple()
+        {
+            var val = new PocoSimple()
+            {
+                Int = 1,
+                Str = "1",
+            };
+
+            var buf = BinaryConvert.SerializeObject(val);
+            var cloned = BinaryConvert.DeserializeObject<PocoSimple>(buf);
+
+            Assert.AreEqual(val, cloned);
+        }
+
+        [TestMethod]
+        public void Test_PocoWithAllPrimitives()
+        {
+            var val = new PocoWithAllPrimitives()
+            {
+                Byte = 1,
+                SByte = -1,
+                Int16 = -2,
+                UInt16 = 2,
+                Int32 = 7,
+                UInt32 = 22221,
+                Int64 = -324561,
+                UInt64 = 2341,
+                Char = '1',
+            };
+
+            var buf = BinaryConvert.SerializeObject(val);
+            var cloned = BinaryConvert.DeserializeObject<PocoWithAllPrimitives>(buf);
+
+            Assert.AreEqual(val, cloned);
+        }
+
+        [TestMethod]
+        public void Test_PocoComplexNotInitialized()
+        {
+            var val = new PocoComplex();
+
+            var buf = BinaryConvert.SerializeObject(val);
+            var cloned = BinaryConvert.DeserializeObject<PocoComplex>(buf);
+
+            Assert.AreEqual(val, cloned);
+        }
+
+        [TestMethod]
+        public void Test_PocoComplex()
+        {
+            var val = new PocoComplex()
+            {
+                Id = 7,
+                Time1 = new DateTime((DateTime.UtcNow.Ticks / TimeSpan.TicksPerSecond) * TimeSpan.TicksPerSecond),
+                Comment = null,//"Bla!",
+                SubRecord = new PocoWithAllPrimitives()
+                {                   
+                    Byte = 1,
+                    SByte = -2,
+                    Int16 = -3,
+                    UInt16 = 4,
+                    Int32 = 5,
+                    UInt32 = 6,
+                    Int64 = 7,
+                    UInt64 = 8,                    
+                    Char = 'a',
+                },
+                Dec1 = 1234567890.954m,
+                Real32 = 17890.9f,
+                Real64 = 167890.1,
+                TestEnum = TestEnum.Val1,
+                IntList = new List<int> { 1, 4, 9, 16 },
+                SubRecordList = new List<PocoWithAllPrimitives>
+                {
+                    new PocoWithAllPrimitives()
+                    {
+                        Byte = 11,
+                        SByte = -12,
+                        Int16 = -13,
+                        UInt16 = 14,
+                        Int32 = 15,
+                        UInt32 = 16,
+                        Int64 = 17,
+                        UInt64 = 18,
+                        Char = 'b',
+                    }
+                },
+                IntDict = new Dictionary<int, int>
+                {
+                    { 0 , 0 },
+                    { 1 , 1 },
+                    { 2 , 4 },
+                    { 3 , 9 },
+                    { 4 , 16 },
+                },
+                SubRecordDict = new Dictionary<string, PocoSimple>
+                {
+                    { "key1" , new PocoSimple{Int = 1 , Str = null}},
+                    { "key2" , new PocoSimple{Int = -1 , Str = "str"} },
+                }
+            };
+
+            var buf = BinaryConvert.SerializeObject(val);
+            var cloned = BinaryConvert.DeserializeObject<PocoComplex>(buf);
+
+            Assert.AreEqual(val, cloned);
+        }
+
+    }
+}
